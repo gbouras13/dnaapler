0c1ca2d2e2d548efc01e752fa9af7e61ff79d3cd
gbouras13 <george.bouras@adelaide.edu.au>
2023-11-07 11:52:42 +1030

    address #52 to remove stderr print bug

--- src/dnaapler/utils/external_tools.py
+++ src/dnaapler/utils/external_tools.py
@@ -46,14 +46,13 @@ class ExternalTool:
 
     def run(self) -> None:
         with open(self.out_log, "w") as stdout_fh, open(self.err_log, "w") as stderr_fh:
-            print(f"Command line: {self.command_as_str}", file=stderr_fh)
             logger.info(f"Started running {self.command_as_str} ...")
             self._run_core(self.command, stdout_fh=stdout_fh, stderr_fh=stderr_fh)
             logger.info(f"Done running {self.command_as_str}")
 
     @staticmethod
     def _run_core(command: List[str], stdout_fh, stderr_fh) -> None:
-        subprocess.check_call(command, stdout=stdout_fh, stderr=stderr_fh)
+        subprocess.call(command, stdout=stdout_fh, stderr=stderr_fh)
 
     @staticmethod
     def run_tools(
